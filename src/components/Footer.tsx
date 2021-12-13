d83943fc1a9c50287961128edbee87e64aa55078
Joe McClintock <joseph@nativemeadow.com>
2021-12-12 18:16:12 -0800

    footer items added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,21 @@ import classes from './Footer.module.css';
 
 const Footer: React.FC = () => {
   return ( 
-    <footer className={classes['main-footer']}>
-    <h1>Footer</h1>
+    <footer className={classes['layout']}>
+        <div className={classes['wrapper']}>
+          <div className={classes['content']}>
+            <ul className={classes['items']}>
+              <li><a className={classes['link']} href="#">Terms &amp; Conditions</a></li>
+              <li>|</li>
+              <li><a className={classes['link']} href="#">Privacy Policy</a></li>
+              <li>|</li>
+              <li><a className={classes['link']} href="#">Site Map</a></li>
+            </ul>
+            <div>
+                Copyright © 2021 Lyngso Garden Materials, Inc. All Rights Reserved.
+            </div>
+          </div>
+        </div>
     </footer>
   );
 }
